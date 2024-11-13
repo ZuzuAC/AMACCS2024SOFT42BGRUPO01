fd7c67ca209728f84c16bc6aa05fd10834b64cb5
Azucena Castillo <150406544+ZuzuAC@users.noreply.github.com>
2024-11-13 14:08:55 -0600

    first commit

---
+++ Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+const BASE_URL = "http://localhost/tienda-youtube/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "";
+const USER_SMTP = "Tu correo";
+const PASS_SMTP = "contraseña generada para esta aplicacion";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
