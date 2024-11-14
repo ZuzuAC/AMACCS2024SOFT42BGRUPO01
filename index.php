e7cc655dc241ba33501d36581fd4c8b5e477e310
Azucena Castillo <150406544+ZuzuAC@users.noreply.github.com>
2024-11-13 23:35:19 -0600

    Correccion de Password

--- index.php
+++ index.php
@@ -1,35 +1,69 @@
-<?php
-require_once 'Config/Config.php';
-$ruta = !empty($_GET['url']) ? $_GET['url'] : "home/index";
-$array = explode("/", $ruta);
-$controller = ucfirst($array[0]);
-$metodo = "index";
-$parametro = "";
-if (!empty($array[1])) {
-    if (!empty($array[1] != "")) {
-        $metodo = $array[1];
-    }
-}
-if (!empty($array[2])) {
-    if (!empty($array[2] != "")) {
-        for ($i = 2; $i < count($array); $i++) {
-            $parametro .= $array[$i] . ",";
-        }
-        $parametro = trim($parametro, ",");
-    }
-}
-require_once 'Config/App/Autoload.php';
-require_once 'Config/Helpers.php';
-$dirControllers = "Controllers/" . $controller . ".php";
-if (file_exists($dirControllers)) {
-    require_once $dirControllers;
-    $controller = new $controller();
-    if (method_exists($controller, $metodo)) {
-        $controller->$metodo($parametro);
-    } else {
-        header('Location: '.BASE_URL.'errors');
-    }
-} else {
-    header('Location: ' . BASE_URL . 'errors');
-}
-?>
+<?php include_once 'Views/template/header-admin.php'; ?>
+
+<button class="btn btn-primary mb-2" type="button" id="nuevo_registro">Nuevo</button>
+
+<div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover" style="width: 100%;" id="tblUsuarios">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombres</th>
+                        <th>Apellidos</th>
+                        <th>Correo</th>
+                        <th>Foto</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<div id="nuevoModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="my-modal-title" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title" id="titleModal"></h5>
+                <button class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                </button>
+            </div>
+            <form id="frmRegistro">
+                <div class="modal-body">
+                    <input type="hidden" id="id" name="id">
+                    <div class="form-group mb-2">
+                        <label for="nombre">Nombres</label>
+                        <input id="nombre" onkeypress="return soloTexto(event)" class="form-control" type="text" name="nombre" placeholder="Nombres">
+                    </div>
+                    <div class="form-group mb-2">
+                        <label for="apellido">Apellidos</label>
+                        <input id="apellido" onkeypress="return soloTexto(event)" class="form-control" type="text" name="apellido" placeholder="Apellidos">
+                    </div>
+                    <div class="form-group mb-2">
+                        <label for="correo">Correo</label>
+                        <input id="correo" onblur="validarCorreo()" class="form-control" type="email" name="correo" placeholder="Correo Electrónico">
+                    </div>
+                    <div class="form-group mb-2">
+                        <label for="clave">Contraseña</label>
+                        <input id="clave" class="form-control" type="password" name="clave" placeholder="Contraseña">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" type="submit" id="btnAccion">Registrar</button>
+                    <button class="btn btn-danger" type="button" data-bs-dismiss="modal">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'Views/template/footer-admin.php'; ?>
+
+<script src="<?php echo BASE_URL . 'assets/js/modulos/usuarios.js'; ?>"></script>
+
+</body>
+
+</html>
